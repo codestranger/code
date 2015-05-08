ac7b8451a70ab3304a1da42c45a3912af9764dfb
zynick <zynick@foxmail.com>
2015-05-08 09:42:36 +0800

    启动页面改为登录注册按钮

---
+++ app/src/main/java/com/nickzy/codestranger/Start_activity.java
@@ -0,0 +1,17 @@
+package com.nickzy.codestranger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by define on 2015/5/7.
+ */
+public class Start_activity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+    }
+}
